e418fdf5f497021784579a95e1923bd8895d9562
Hungpvhd <hungpvhd00529@fpt.edu.vn>
2019-10-12 20:39:51 +0700

    Maybe..

--- src/main/java/t1708e/example/controller/BookController.java
+++ src/main/java/t1708e/example/controller/BookController.java
@@ -5,20 +5,45 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import t1708e.example.entity.BookDetails;
+import t1708e.example.repository.BookDeatilsRepository;
 import t1708e.example.service.BookService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/book")
+@RequestMapping(value = {"/book" , "/"})
 public class BookController {
     @Autowired
     BookService bookService;
+
+    @Autowired
+    BookDeatilsRepository bookDeatilsRepository;
     @RequestMapping(method = RequestMethod.GET)
     public String index(Model model) {
         List<BookDetails> bookDetails = bookService.bookDetails();
         model.addAttribute("bookDetails", bookDetails);
         return "index";
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public String search(Model model){
+        List<BookDetails> books = new ArrayList<>();
+        model.addAttribute("books", books);
+        return "search";
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.POST)
+    public String searchBook(Model model, @RequestParam("keyword") String keyword){
+        if (keyword.isEmpty()){
+            List<BookDetails> bookDetails = new ArrayList<>();
+            model.addAttribute("listBook",bookDetails);
+            return "Search";
+        }
+        List<BookDetails> books = bookDeatilsRepository.searchBookByName(keyword);
+        model.addAttribute("books", books);
+        return "search";
     }
 }
--- src/main/java/t1708e/example/repository/BookDeatilsRepository.java
+++ src/main/java/t1708e/example/repository/BookDeatilsRepository.java
@@ -1,7 +1,14 @@
 package t1708e.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import t1708e.example.entity.BookDetails;
 
+import java.util.List;
+
 public interface BookDeatilsRepository extends JpaRepository<BookDetails, Integer> {
+
+    @Query("select n from BookDetail as n where n.name like %:name% or n.author like %:name% or n.genre like %:name%")
+    List<BookDetails> searchBookByName(@Param("name") String keyword);
 }
